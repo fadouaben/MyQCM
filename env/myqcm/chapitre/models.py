ee60462600a1d024bede3e4131335bc4dc64c4d0
fadouaben <fadouabenhajyedder@gmail.com>
2023-08-19 21:00:00 +0200

    cardénalités

--- env/myqcm/chapitre/models.py
+++ env/myqcm/chapitre/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from user.models import UserClass
 
 # Create your models here.
 class Chapitre(models.Model):
     titre = models.CharField(max_length=100)
-
+    teacher = models.ForeignKey(UserClass,on_delete=models.CASCADE,null=True,blank=True,limit_choices_to={'role':'TEACHER'})
+    student = models.ManyToManyField(UserClass,related_name='chapitres_suivis', blank=True,limit_choices_to={'role':'STUDENT'})
 
 class Score(models.Model):
     chapitre = models.ForeignKey(Chapitre, on_delete=models.CASCADE)
-   #utilisateur = models.ForeignKey(UserClass, on_delete=models.CASCADE)
+    utilisateur = models.ForeignKey(UserClass, on_delete=models.CASCADE)
     score = models.IntegerField()
+
+
--- env/myqcm/myqcm/settings.py
+++ env/myqcm/myqcm/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'myqcm/static')
+    os.path.join(BASE_DIR,'myqcm\static')
 ]
 
 # Default primary key field type
